073e61385ca84d87b8715dd87e74a411def082d7
mustafaelgazzar <mustafaelgazzar@gmail.com>
2023-10-07 05:14:40 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entty function
+ *
+ * Description: Print single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int 1;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
