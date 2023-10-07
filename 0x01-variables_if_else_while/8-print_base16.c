3582d74dcf45e7cb1cb24722bed9429afa6799d7
mustafaelgazzar <mustafaelgazzar@gmail.com>
2023-10-07 07:25:57 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main - entry point
  *
- * Description:prints all the numbers of base 16 in lowercase
+ * Description: prints all the numbers of base 16 in lowercase
  *
  * Return: Always (0) (success)
  */
@@ -13,9 +13,16 @@ int main(void)
 	char x;
 
 	for (x = '0'; x <= '9'; x++)
-		putchar(x + '0');
-	for (x = 'a'; x <= 'f'; x++)
+	{
 		putchar(x);
+	}
+
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
