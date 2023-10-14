382e45b70ee4b59fc540949f150427b4532325e9
mustafaelgazzar <mustafaelgazzar@gmail.com>
2023-10-14 05:00:30 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase charachter
+ * @c: Function parameter
+ * Return: 1 for success and 0 for failure
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
